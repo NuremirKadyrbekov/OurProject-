938acb99f5c5b13fbadb45f81bb20c33df25b5ad
ishembaevvv <ishembaevvv05@gmail.com>
2023-12-27 18:36:02 +0300

    update project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Ayat } from "./components/Ayat/Ayat";
 import Nuremir from "./components/Nuremir/Nuremir";
 import { Alina } from "./components/Alina/Alina";
+import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="App flex justify-center items-center h-screen">
+    <div className="App">
       Hello World!
       <Ayat />
       <Nuremir />
       <Alina />
+      <Counter />
     </div>
   );
 }
